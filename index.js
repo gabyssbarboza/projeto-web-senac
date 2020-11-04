51f341da2a28dbc10b5c4575c0a3a43d8d6050d9
gabys barboza <gabrielly.barboza@outlook.com>
2020-11-04 19:39:30 -0300

    page: about us

---
+++ index.js
@@ -0,0 +1,27 @@
+
+ScrollReveal().reveal('.container-text', {
+    easing: 'ease-in',
+    delay: 200,
+    origin: 'rigth',
+    distance: '60px'
+});
+
+
+
+ScrollReveal().reveal('.container-middle', {
+    easing: 'ease-in',
+    reset: true, 
+    delay: 500,
+    origin: 'top',
+    distance: '60px'
+});
+
+
+
+ScrollReveal().reveal('.container-bottom-img', {
+    easing: 'ease-in',
+    reset: true,
+    delay: 600,
+    origin: 'left',
+    distance: '60px'
+});
